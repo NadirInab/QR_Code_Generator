0d13baa51f63fee218a95b1b2c8310ed82d17266
nadir inab <nadir.inab.dev@gmail.com>
2023-02-24 22:56:33 +0100

    navbar is well styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function navbar() {
         <>
             <Navbar id='Navbar'  className='container p-3' expand="lg" variant="light" bg="muted">
                 <Container>
-                        <h2 className='w-100 mx-auto' >QR Code Generator</h2>
+                        <h2 id='hero' className='w-100 mx-auto' >QR Code Generator</h2>
                 </Container>
             </Navbar>
         </>
